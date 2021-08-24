91b2a1015dba03f894d3614b9ceba9f89ee3b91b
Herul Syahwandi S <34206362+galontong@users.noreply.github.com>
2021-08-24 11:26:15 +0700

    Update Program.cs

--- BasicGit55/BasicGit55/Program.cs
+++ BasicGit55/BasicGit55/Program.cs
@@ -10,5 +10,61 @@ namespace BasicGit55
             Console.WriteLine("Hello World! dari herul syahwandi");
             Console.WriteLine("MCC ASP.NET");
         }
+
+        public void ShowHerul()
+        {
+        }
+        
+        public void ShowAchmad()
+        {
+        }
+        
+        public void ShowErche()
+        {
+        }
+        
+        public void ShowFebi()
+        {
+        }
+        
+        public void ShowFerdi()
+        {
+        }
+        
+        public void ShowHasbi()
+        {
+        }
+        
+        public void ShowJona()
+        {
+        }
+        
+        public void ShowKevin()
+        {
+        }
+        
+        public void ShowLalak()
+        {
+        }
+        
+        public void ShowLinggom()
+        {
+        }
+        
+        public void ShowMartin()
+        {
+        }
+        
+        public void ShowOcta()
+        {
+        }
+        
+        public void ShowNaufal()
+        {
+        }
+        
+        public void ShowYepta()
+        {
+        }
     }
 }
